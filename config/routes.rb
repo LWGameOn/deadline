69e4d9c17eb77f4b798eda5a279b52794d31ebe6
Elisabeth Messmer <elisabethsophiem@web.de>
2023-02-27 15:22:16 +0100

    basic routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: "hitmen#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :hitmen, only: %i[index show] do
+    resources :jobs, only: %i[index show create update destroy]
+  end
 end
