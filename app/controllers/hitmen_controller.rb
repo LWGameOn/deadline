ae6cbec1b2dc8b68a41cee9419bad1ac3a12d40d
Vittoria Benedeto <vbenedetoamerico@gmail.com>
2023-02-28 14:24:36 +0100

    link_to hitmans profile

--- app/controllers/hitmen_controller.rb
+++ app/controllers/hitmen_controller.rb
@@ -1,12 +1,11 @@
 class HitmenController < ApplicationController
-  before_action :set_hitman
+  before_action :set_hitman, only: :show
 
   def index
     @hitmen = Hitman.all
   end
 
   def show
-    @hitman = Hitman.find(params[:id])
   end
 
   private
