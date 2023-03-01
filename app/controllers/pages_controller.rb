0ac78a8504e7b3d3702582258cb85e98b332d4ea
Elisabeth Messmer <elisabethsophiem@web.de>
2023-03-01 13:52:44 +0100

    more stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def index
+    # Index jobs the user's hitmen are assigned to
+    @hitman = Hitman.find_by user: current_user
+    @jobs = Job.where(hitman: @hitman)
+  end
 end
