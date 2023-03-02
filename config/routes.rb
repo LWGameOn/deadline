29254fb64f35747031afc1d8b3c0a87ee0149d17
Elisabeth Messmer <elisabethsophiem@web.de>
2023-03-02 12:06:30 +0100

    dashboard hitman profile

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
 
   # get '/jobs/requests', to: 'jobs#index_requests', as: 'requests'
 
-  resources :hitmen, only: %i[index show new create] do
+  resources :hitmen, only: %i[index show new create edit update destroy] do
     resources :jobs, only: %i[new create]
   end
+
+  # delete 'hitmen/destroy', to: 'hitmen#destroy', as: 'delete'
 
   resources :jobs, only: %i[index show update destroy edit]
 
