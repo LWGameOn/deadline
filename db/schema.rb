e30bf319c76547dd872b38b1c1bb094c2d30e54d
Stefan Bachmann <stefan.bachmann123@gmail.com>
2023-03-03 12:10:04 +0100

    added default value for hitman price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_095345) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_110334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_095345) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.decimal "price"
+    t.decimal "price", default: "0.0"
     t.index ["user_id"], name: "index_hitmen_on_user_id"
   end
 
